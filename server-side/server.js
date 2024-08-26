6f7dc1447a1dc37d3a46b7088e7e9bd962f33c79
Rajat N <snautiyal048@gmail.com>
2024-08-26 13:46:11 +0530

    Update server.js

--- server-side/server.js
+++ server-side/server.js
@@ -26,8 +26,6 @@ import { sendMessage} from './controller/message.js'
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'client/build')));
-
 app.use(cors({ origin: ['http://localhost:5173'],credentials: true } ));
 const server = createServer(app);
 const io = new Server(server, {
@@ -69,10 +67,6 @@ app.post('/message/send/:id',upload.single('file'),sendMessage); //friend id
 app.post('/post/story',authUser, upload.single('image'), createStory);
 
 const PORT = process.env.PORT || 5000;
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'));
-});
 
 server.listen(PORT, () => {
     console.log(`Server connected on port ${PORT}`);
